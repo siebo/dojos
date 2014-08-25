38b70c078180501ff94b9f263f5b3ef468eb743f
David Siedband <david@zentraal.com>
2014-08-25 06:38:32 -0300

    test for list view

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.test.client import Client
+from django.test.client import RequestFactory
 
 from .models import Dojo
 
@@ -11,3 +13,17 @@ class DojoTests(TestCase):
             str(contact),
             'Takemusu Aikido',
     )
+
+class DojoListViewTests(TestCase):
+    """Dojo list view tests."""
+    
+    def test_dojos_in_the_context(self): 
+      
+      client = Client()
+      response = client.get('/')
+      
+      self.assertEquals(list(response.context['object_list']), [])
+      
+      Dojo.objects.create(name='Aikido Amsterdam')
+      response = client.get('/')
+      self.assertEquals(response.context['object_list'].count(), 1)
