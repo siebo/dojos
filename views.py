c7b0059d319184111a6196d1cad2087181cde6e5
David Siedband <david@zentraal.com>
2014-08-25 06:25:59 -0300

    simple CBV-based add form

--- views.py
+++ views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import ListView 
+from django.views.generic import ListView
+from django.views.generic import CreateView
+
 from .models import Dojo
 
 class ListDojosView(ListView):
     model = Dojo
-    
     template_name = 'dojo_list.html'
+
+class CreateDojoView(CreateView):
+    model = Dojo
+    template_name = 'edit_dojo.html'
+
+    def get_success_url(self):
+        return reverse('dojos-list')
