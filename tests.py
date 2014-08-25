feca2f48b6e667d547428762817529396840a678
David Siedband <david@zentraal.com>
2014-08-25 06:42:41 -0300

    added test case for context request factory

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ from django.test.client import Client
 from django.test.client import RequestFactory
 
 from .models import Dojo
+from .views import ListDojosView
 
 class DojoTests(TestCase):
     """Dojo model tests."""
@@ -27,3 +28,16 @@ class DojoListViewTests(TestCase):
       Dojo.objects.create(name='Aikido Amsterdam')
       response = client.get('/')
       self.assertEquals(response.context['object_list'].count(), 1)
+      
+    def test_dojos_in_the_context_request_factory(self):
+  
+        factory = RequestFactory()
+        request = factory.get('/')
+
+        response = ListDojosView.as_view()(request)
+
+        self.assertEquals(list(response.context_data['object_list']), [])
+
+        Dojo.objects.create(name='Aikido Sao Paulo')
+        response = ListDojosView.as_view()(request)
+        self.assertEquals(response.context_data['object_list'].count(), 1)
