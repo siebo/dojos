8f2334decf0824b1a23ed300e6b50b63fe310a73
David Siedband <david@zentraal.com>
2014-08-25 06:10:34 -0300

    simple view and template

--- views.py
+++ views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView 
+from .models import Dojo
 
-# Create your views here.
+class ListDojosView(ListView):
+    model = Dojo
+    
+    template_name = 'dojo_list.html'
